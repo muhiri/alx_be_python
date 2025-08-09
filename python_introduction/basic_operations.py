e42e978cf1909092fa34cb6fe9f5738d7fa0ea5e
muhiri <gilbertmuhiri29@gmail.com>
2025-08-09 09:58:11 +0300

    my first python assignment

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10
+number2 = 5
+print ("The sum of", number1, "and", number2, "is", number1 + number2)
+print ("The difference between", number1, "and", number2, "is", number1 - number2)
+print ("The product of", number1, "and", number2, "is", number1 * number2)
